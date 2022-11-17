c58d9992ac1b550f5cff5d1ffe3c926a7ac1afd5
tomekk <jansen@hnm.ch>
2022-11-17 11:00:17 +0100

    php 8

--- src/app/bootstrap/ui/BsConfig.php
+++ src/app/bootstrap/ui/BsConfig.php
@@ -18,8 +18,8 @@ class BsConfig {
 	protected $formCheckAttrs;
 
 	public function __construct(bool $required, bool $autoPlaceholder, ?string $placeholder,
-			string|UiComponent|null $helpText, bool $labelHidden, array $labelAttrs, array $controlAttrs, array $groupAttrs,
-			array $rowClassNames = null, BsComposer $child = null, array $formCheckAttrs) {
+			$helpText = null, bool $labelHidden = false, array $labelAttrs = null, array $controlAttrs = null, array $groupAttrs = null,
+			array $rowClassNames = null, BsComposer $child = null, array $formCheckAttrs = null) {
 		$this->required = $required;
 		$this->autoPlaceholder = $autoPlaceholder;
 		$this->placeholder = $placeholder;
